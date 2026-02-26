31b5f58a7cfa2d86b1c71e10522e9565000ec614
Caio <caiosalmarques7@gmail.com>
2026-02-26 14:39:07 -0300

    Delete exercicio3 directory

--- exercicio3/q2.py
+++
@@ -1,6 +0,0 @@
-num = int(input("digite um numero positivo: "))
-aux = 0
-
-while aux <= num:
-    print(aux)
-    aux = aux + 2
--- exercicio3/q4.py
+++
@@ -1,9 +0,0 @@
-num = int(input("digite um numero (0 para encerrar): "))
-
-while num != 0:
-    m = num
-    while m < 100:
-        print(m)
-        m += num
-    
-    num = int(input("digite um numero (0 para encerrar): "))
