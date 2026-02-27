1991f5cc38003d74a8748c63466f55f99b2cfc8f
Caio <caiosalmarques7@gmail.com>
2026-02-27 08:19:46 -0300

    Delete exercicios/exercicio3/q8.py

--- exercicios/exercicio3/q8.py
+++
@@ -1,9 +0,0 @@
-num = float(input("digite um numero decimal: "))
-binario = ""
-
-while num > 0:
-    binario = str(num % 2) + binario
-    num = num // 2
-while len(binario) == 0:
-    binario = "0"
-print(f"binário: {binario}")
