280ffc322946da4484d8eaf56dfc53296439c7f3
Caio <caiosalmarques7@gmail.com>
2026-02-27 15:48:52 -0300

    Delete exercicios/exercicio2/q3.py

--- exercicios/exercicio2/q3.py
+++
@@ -1,6 +0,0 @@
-numero = int(input("digite um numero inteiro: "))
-
-if 50 <= numero <= 100:
-    print("entre 50 e 100")
-else:
-    print("menor que 0 ou maior que 200") if numero < 0 or numero > 200 else print("fora das condições anteriores")
